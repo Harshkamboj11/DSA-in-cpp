f93e69abf122345ccc3659f02b9c5913ab1ff1f3
harshkamboj11 <hk8994854@gmail.com>
2025-06-07 10:25:47 +0530

    Array3

--- Arrays/array3.cpp
+++ Arrays/array3.cpp
@@ -1,31 +1,36 @@
-//LINEAR SEARCH
+// LINEAR SEARCH
 
 #include <iostream>
 using namespace std;
- 
-int linearsearch(int arr[] , int size , int target){
-    for (int i=0;i<=size;i++){
-        if (arr[i]==target){
+
+int linearsearch(int arr[], int size, int target)
+{
+    for (int i = 0; i <= size; i++)
+    {
+        if (arr[i] == target)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int main(){
-    int size,target; //size of array
-    cout<<"Enter the size of array: ";
-    cin>>size;
+int main()
+{
+    int size, target; // size of array
+    cout << "Enter the size of array: ";
+    cin >> size;
 
-    int arr[size]={};
-    for (int i=0;i<=size;i++){
-        cout<<"Enter "<<i<<" element: ";
-        cin>>arr[i];
+    int arr[size] = {};
+    for (int i = 0; i <= size; i++)
+    {
+        cout << "Enter " << i << " element: ";
+        cin >> arr[i];
     }
-    cout<<"Enter the target value u want to search in the array: ";
-    cin>>target;
+    cout << "Enter the target value u want to search in the array: ";
+    cin >> target;
 
-    int ans = linearsearch(arr,size,target);
+    int ans = linearsearch(arr, size, target);
 
-    cout<<"The given target is at the index: "<<ans<<endl;
+    cout << "The given target is at the index: " << ans << endl;
 }
