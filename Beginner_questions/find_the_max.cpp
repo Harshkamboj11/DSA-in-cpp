31e97925fb3d55aff1b8e324a5da97f256f5210b
harshkamboj11 <hk8994854@gmail.com>
2025-06-13 17:46:04 +0530

    updated

---
+++ Beginner_questions/find_the_max.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter first number: ";
+    cin>>a;
+    cout<<"Enter second number: ";
+    cin>>b;
+    cout<<""<<endl;
+    if (a>b){
+        cout<<a<<" is greater."<<endl;
+    }
+    else{
+        cout<<b<<" is greater."<<endl;
+    }
+}
