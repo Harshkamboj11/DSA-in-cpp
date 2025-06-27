5662ce4302d03fc31667103c42fec89afb441655
harshkamboj11 <hk8994854@gmail.com>
2025-06-27 11:02:22 +0530

    updated

---
+++ DSA roadmap practice/largest_of_three.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2,n3;
+    cout<<"Enter first number: ";
+    cin>>n1;
+    cout<<"Enter second number: ";
+    cin>>n2;
+    cout<<"Enter third number: ";
+    cin>>n3;
+
+    if(n1>n2 && n1>n3){
+        cout<<n1<<" is greater!!";
+    }
+    else if(n2>n1 && n2>n3){
+        cout<<n2<<"is greater!!";
+    }
+    else if(n3>n1 && n3>n2){
+        cout<<n3<<" is greater!!";
+    }
+
+    return 0;
+}
