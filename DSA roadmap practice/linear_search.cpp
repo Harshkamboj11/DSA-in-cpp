58021bb2ecfbb8a7a70f1c8d407b15aeb80818e6
harshkamboj11 <hk8994854@gmail.com>
2025-07-16 20:09:46 +0530

    updated

--- DSA roadmap practice/linear_search.cpp
+++ DSA roadmap practice/linear_search.cpp
@@ -1,29 +1,43 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-bool find_element(int arr[],int size,int target){
-    //looping through the array search the terget element
-    for(int i=0;i<size;i++){
-        if(arr[i]==target){
-            return 1; //if found the target element 
+bool find_element(vector<int> arr, int target) {
+    int size = arr.size();
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return 1;
         }
     }
-    
-    return 0; //if not found the target element 
+    return 0;
 }
 
-int main(){
-    int num[7]={3,4,2,33,44,5,6};
-    cout<<"Enter the value of element you want to search: ";
-    int find;
-    cin>>find;
-    
-    bool found = find_element(num,7,find);
+void inputArray(vector<int> &arr) {
+    int size;
+    cout << "Enter the number of elements to insert: ";
+    cin >> size;
 
-    if(found){
-        cout<<"Yes,the target element is present in the given array....";
+    int element;
+    for (int i = 0; i < size; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> element; 
+        arr.push_back(element);
     }
-    else{
-        cout<<"No,the given array is not present....";
+}
+
+int main() {
+    vector<int> num;
+    inputArray(num);
+
+    cout << "Enter the value of element you want to search: ";
+    int find;
+    cin >> find;
+
+    bool found = find_element(num, find);
+
+    if (found) {
+        cout << "Yes, the target element is present in the given array....";
+    } else {
+        cout << "No, the given array is not present....";
     }
 }
