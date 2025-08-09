9ee2f298a18f63ba6342569c1fd9c9a98e620a28
harshkamboj11 <hk8994854@gmail.com>
2025-08-09 23:02:18 +0530

    updated

---
+++ pointers/swap_numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp ;
+}
+
+int main(){
+    int a,b;
+
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+
+    swap(&a,&b);
+
+    cout<<"NOW,The value of a and b is "<<a<<" and "<<b<<endl;
+}
